60e35d359b12ecb2cf8adf602a56d283d2ee2c6f
rathod-tirth <tirthrathod6@gmail.com>
2023-03-20 15:57:53 +0530

    C

--- user_tables.c
+++ user_tables.c
@@ -3,12 +3,12 @@
 void main()
 
 {
-    int a;
+    int a,i;
     
     printf("Enter any Number : ");
     scanf("%d", &a);
 
-    for (int i=1; i < 11; i++)
+    for (i=1; i < 11; i++)
     {
         printf("%d x %d = %d\n", a, i, a*i);
     }
